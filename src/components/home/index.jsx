da8a80296bbcca7afdb5c15f42716e50b6fab9f3
codilot <30231189+Codilot@users.noreply.github.com>
2020-12-05 19:21:09 +0100

    Create grid for homepage

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -4,16 +4,19 @@ import { Button, Row, Col, Space } from 'antd';
 
 const Home = () => (
     <Row gutter={[8, { xs: 8, md: 16 }]}>
-        <Col className="gutter-row" offset={2} span={20}>
+        <Col className="gutter-row" xs={{ span: 20, offset: 2 }} lg={{ span: 12, offset: 6 }}>
             <h1 className={"foo"}>Saatgut erhalten durch Vermehrung</h1>
             <p className={"bar"}>My text bla bla bla</p>
+            <Row gutter={[8, 8]}>
+            <Col className="gutter-row" xs={{ span: 20, offset: 2 }} lg={{ span: 12, offset: 6 }}>
+                <Button size="large" type="primary" block><Link to="/register">Register</Link></Button>
+            </Col>
+             <Col className="gutter-row" xs={{ span: 20, offset: 2 }} lg={{ span: 12, offset: 6 }}>   
+                <Button size="large" type="default" block><Link to="/login">Login</Link></Button>
+            </Col>
+            </Row>
         </Col>
-        <Col className="gutter-row" offset={2} span={20}>
-            <Button size="large" type="primary" block><Link to="/register">Register</Link></Button>
-        </Col>
-        <Col className="gutter-row" offset={2} span={20}>   
-            <Button size="large" type="default" block><Link to="/login">Login</Link></Button>
-        </Col>
+        
     </Row>
 )
 
