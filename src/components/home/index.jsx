683fda6f4279880fc838e7e2e9ca934ad31ecbfa
codilot <30231189+Codilot@users.noreply.github.com>
2020-12-07 08:33:54 +0100

    Grid improvements

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -7,17 +7,17 @@ const { Text } = Typography;
 
 const Home = () => (
     <PageLayout>
-        <Space style={{padding: "24px 0", textAlign: "center"}} direction="vertical">
-            <Headline>Saatgut erhalten<br /> durch Vermehrung</Headline>
-            <Text strong>Schutzen Sie die Vielfalt durch den Anbau von traditionellen Gemüsesorten und der Ernte eigenes Saatgut</Text>
-        </Space>
-        <Row gutter={[8, 8]}>
-        <Col className="gutter-row" xs={{ span: 24 }} lg={{ span: 12, offset: 6 }}>
-            <Button size="large" type="primary" block><Link to="/register">Register</Link></Button>
-        </Col>
-        <Col className="gutter-row" xs={{ span: 24 }} lg={{ span: 12, offset: 6 }}>   
-            <Button size="large" type="default" block><Link to="/login">Login</Link></Button>
-        </Col>
+        <Row gutter={[8, 8]} >
+            <Col style={{padding: "24px 0", textAlign: "center"}} className="gutter-row" xs={{ span: 24 }} lg={{ span: 12, offset: 6 }}>
+                <Headline>Saatgut erhalten<br /> durch Vermehrung</Headline>
+                <Text type="secondary">Schutzen Sie die Vielfalt durch den Anbau von traditionellen Gemüsesorten und der Ernte eigenes Saatgut</Text>
+            </Col>
+            <Col className="gutter-row" xs={{ span: 24 }} lg={{ span: 12, offset: 6 }}>
+                <Button shape="round" size="large" type="primary" block><Link to="/register">Register</Link></Button>
+            </Col>
+            <Col className="gutter-row" xs={{ span: 24 }} lg={{ span: 12, offset: 6 }}>   
+                <Button shape="round" size="large" type="default" block><Link to="/login">Login</Link></Button>
+            </Col>
         </Row>
     </PageLayout>
 )
--- src/components/page_layout/index.jsx
+++ src/components/page_layout/index.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import { Row, Col } from "antd";
 
 const PageLayout = ({ children }) => (
-    <Row gutter={[8, { xs: 8, md: 16 }]}>
+    <Row>
         <Col
-            className="gutter-row"
             xs={{ span: 20, offset: 2 }}
             lg={{ span: 12, offset: 6 }}
         >
